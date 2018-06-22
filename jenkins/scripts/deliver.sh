75af75076fd2a5895da0963d3ca24157e26786f1
nate01776 <nathan.wright@smartbear.com>
2018-06-22 15:17:03 -0400

    stripping out delivery excess

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,28 +1,30 @@
 #!/usr/bin/env sh
 
-echo 'The following "npm" command builds your Node.js/React application for'
-echo 'production in the local "build" directory (i.e. within the appropriate'
-echo 'subdirectory of "/var/jenkins_home/workspace/"), correctly bundles React'
-echo 'in production mode and optimizes the build for the best performance.'
-set -x
-npm run build
-set +x
+# echo 'The following "npm" command builds your Node.js/React application for'
+# echo 'production in the local "build" directory (i.e. within the appropriate'
+# echo 'subdirectory of "/var/jenkins_home/workspace/"), correctly bundles React'
+# echo 'in production mode and optimizes the build for the best performance.'
+# set -x
+# npm run build
+# set +x
 
-echo 'The following "npm" command runs your Node.js/React application in'
-echo 'development mode and makes the application available for web browsing.'
-echo 'The "npm start" command has a trailing ampersand so that the command runs'
-echo 'as a background process (i.e. asynchronously). Otherwise, this command'
-echo 'can pause running builds of CI/CD applications indefinitely. "npm start"'
-echo 'is followed by another command that retrieves the process ID (PID) value'
-echo 'of the previously run process (i.e. "npm start") and writes this value to'
-echo 'the file ".pidfile".'
+# echo 'The following "npm" command runs your Node.js/React application in'
+# echo 'development mode and makes the application available for web browsing.'
+# echo 'The "npm start" command has a trailing ampersand so that the command runs'
+# echo 'as a background process (i.e. asynchronously). Otherwise, this command'
+# echo 'can pause running builds of CI/CD applications indefinitely. "npm start"'
+# echo 'is followed by another command that retrieves the process ID (PID) value'
+# echo 'of the previously run process (i.e. "npm start") and writes this value to'
+# echo 'the file ".pidfile".'
 set -x
-npm start &
-sleep 1
-echo $! > .pidfile
+npm start 
 set +x
+# &
+# sleep 1
+# echo $! > .pidfile
+# set +x
 
-echo 'Now...'
-echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
-echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'
-echo 'created your initial Pipeline as a Jenkinsfile.)'
+# echo 'Now...'
+# echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
+# echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'
+# echo 'created your initial Pipeline as a Jenkinsfile.)'
