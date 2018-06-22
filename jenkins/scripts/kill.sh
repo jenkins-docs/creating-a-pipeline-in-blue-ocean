1c8a6afaf234a8563fa972cd65b340042634b1fe
nate01776 <nathan.wright@smartbear.com>
2018-06-22 16:23:24 -0400

    tweaked comments to reflect updated process

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 
-# echo 'The following command terminates the "npm start" process using its PID'
-# echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
-# echo 'was executed.'
+echo 'The following command terminates the "npm start" process using forever'
+
 set -x
 forever stopall
 set +x
