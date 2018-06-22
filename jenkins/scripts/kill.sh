3a91c807e123d5c0187942d2163ac262040de1c8
nate01776 <nathan.wright@smartbear.com>
2018-06-22 16:17:01 -0400

    removing old teardown process

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
-echo 'The following command terminates the "npm start" process using its PID'
-echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
-echo 'was executed.'
+# echo 'The following command terminates the "npm start" process using its PID'
+# echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
+# echo 'was executed.'
 set -x
-kill $(cat .pidfile)
+forever stopall
+set +x
