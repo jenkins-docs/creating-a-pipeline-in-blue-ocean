3fe49951c683927c9ccc46b48761c2bdc25ab53e
nWrightSB <30868289+nWrightSB@users.noreply.github.com>
2018-06-22 15:11:09 -0400

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -4,9 +4,9 @@ echo 'The following "npm" command builds your Node.js/React application for'
 echo 'production in the local "build" directory (i.e. within the appropriate'
 echo 'subdirectory of "/var/jenkins_home/workspace/"), correctly bundles React'
 echo 'in production mode and optimizes the build for the best performance.'
-set -x
-npm run build
-set +x
+# set -x
+# npm run build
+# set +x
 
 echo 'The following "npm" command runs your Node.js/React application in'
 echo 'development mode and makes the application available for web browsing.'
